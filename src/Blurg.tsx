8f19dea4ba1a765319b518010c529408be700aa9
Caleb Taylor <aquaductape@gmail.com>
2020-08-31 21:50:17 -0700

    update

--- src/Blurg.tsx
+++ src/Blurg.tsx
@@ -1,10 +1,19 @@
 import React, { ReactChild, ReactChildren, useEffect, useRef } from "react";
 import { IOS } from "./browserInfo";
 
+if (IOS) {
+  if (typeof window !== undefined) {
+    const html = document.querySelector("html")!;
+    html.style.cursor = "pointer";
+    html.style.webkitTapHighlightColor = "rgba(0, 0, 0, 0)";
+  }
+}
+
 type BlurgProps = {
   children: ReactChild | ReactChildren | boolean | null;
   onBlur: (e: React.FocusEvent) => void;
 };
+
 const Blurg = ({ children, onBlur }: BlurgProps) => {
   useEffect(() => {
     // mainRef.current?.focus();
@@ -24,23 +33,27 @@ const Blurg = ({ children, onBlur }: BlurgProps) => {
   const attr = {} as any;
 
   // if (IOS) {
-  attr.onMouseOut = (e: React.FocusEvent) => {
+  // attr.onMouseOut = (e: React.FocusEvent) => {
+  //   if (!e.currentTarget.contains(e.relatedTarget as Node)) {
+  //     console.log("not related!", e.relatedTarget);
+  //     onBlur(e);
+  //   }
+  // };
+  // } else {
+  attr.onBlur = (e: React.FocusEvent) => {
     if (!e.currentTarget.contains(e.relatedTarget as Node)) {
       console.log("not related!", e.relatedTarget);
       onBlur(e);
     }
   };
-  // } else {
-  //   attr.onBlur = (e: React.FocusEvent) => {
-  //     if (!e.currentTarget.contains(e.relatedTarget as Node)) {
-  //       console.log("not related!", e.relatedTarget);
-  //       onBlur(e);
-  //     }
-  //   };
   // }
 
   return (
-    <div tabIndex={0} {...attr}>
+    <div
+      // onTouch
+      tabIndex={0}
+      {...attr}
+    >
       {children}
     </div>
   );
